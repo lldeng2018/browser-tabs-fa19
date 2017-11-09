54a341b868c3c48d4390fab6277dfbff66b97153
Brandon Myers <brandon-d-myers@uiowa.edu>
2017-11-08 21:33:22 -0600

    handle not ticket found case

--- src/apps/AirportSecurityDatabase.java
+++ src/apps/AirportSecurityDatabase.java
@@ -28,7 +28,9 @@ public class AirportSecurityDatabase {
 	/* check that the ticketNumber matches the name and print OK or DENIED */
 	public void check(int ticketNumber, String nameOnID) {
 		String registeredName = allowedPassengers.get(ticketNumber);
-		if (registeredName.equals(nameOnID)) {
+		if (registeredName == null) {
+			System.out.println("ERROR: "+ticketNumber+" not recognized");
+		} else if (registeredName.equals(nameOnID)) {
 			System.out.println(nameOnID+" okay");
 		} else {
 			System.out.println("ERROR: "+nameOnID+" denied");
@@ -40,7 +42,7 @@ public class AirportSecurityDatabase {
 
 		// tickets are registered
 		int birdTicket = db.register("Bird");
-		int frogTicket = db.register(new String("Frog"));
+		int frogTicket = db.register("Frog");
 		int catTicket = db.register("Cat");
 		int giraffeTicket = db.register("Giraffe");
 
@@ -48,7 +50,7 @@ public class AirportSecurityDatabase {
 		int snakeTicket = birdTicket;
 
 		// passengers try to get through security	
-		db.check(frogTicket, new String("Frog"));
+		db.check(frogTicket, "Frog");
 		db.check(giraffeTicket, "Giraffe");
 		db.check(snakeTicket, "Snake");
 	}		
