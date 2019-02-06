8a8a4b1e3401b59c89891960a79be60eaf72ba09
Brandon Myers <brandon-d-myers@uiowa.edu>
2019-02-06 17:51:08 -0600

    actually remove goodrich most of the code

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -20,7 +20,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package net.datastructures;
 
 import java.util.Iterator;
 import java.util.NoSuchElementException;
